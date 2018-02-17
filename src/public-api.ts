9ff1d390a4b9ae48563da7504aca188d201b3314
David Kudera <kudera.d@gmail.com>
2018-02-18 00:26:01 +0100

    Export all public classes

--- src/public-api.ts
+++ src/public-api.ts
@@ -1,2 +1,7 @@
+export {Command, CommandEnvList} from './command';
+export {EnvironmentVariable} from './environment-variable';
 export {Imba} from './imba';
-export {ScriptMode} from './script';
+export {Input, InputOptions} from './input';
+export {Project} from './project';
+export {RunContext} from './run-context';
+export {Script, ScriptMode} from './script';
